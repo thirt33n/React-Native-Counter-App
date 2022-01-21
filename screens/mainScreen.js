d8a6c5561b8815e80bb10e11b533698f82f778d2
Sid <siddharthpradeep4602@gmail.com>
2022-01-21 21:18:06 +0530

    2nd

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React,{ useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Button } from 'react-native-web';
 
 export default function MainScreen(){
     
@@ -12,6 +13,7 @@ export default function MainScreen(){
                 <LinearGradient colors={['#2B1B39','#1D1835','#1A1A37']}
                 style={{ flex:1,alignItems:'center',
                 justifyContent:'center', }}>
+                    <Text style={styles.info}> ULTIMATE COUNTER </Text>
                     <View style={styles.board}>
                         <Text style={{ fontSize: 90,color:'#FF2281',marginTop:'100%',textAlign: 'center'}}>{count}</Text>
                     </View>
@@ -23,11 +25,17 @@ export default function MainScreen(){
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        color: '#0062FF',
     },
     board: {
         flex: 1,
         width: '50%',
 
+    },
+    info: {
+        color: '#0062FF',
+        marginTop: '20%',
+        fontSize: 30,
+        fontFamily: 'monospace',
+        fontWeight: 'bold',
     }
 })
